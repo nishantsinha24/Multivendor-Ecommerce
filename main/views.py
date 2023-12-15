28b1a5abd21c0b94ceaa0c4b673d9ecc4cf52cd6
nishantsinha24 <nishantsinha1024@gmail.com>
2023-12-15 15:02:37 +0530

    added new module

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from . import models
+from . import serializers
 
 # Create your views here.
+class VendorList(generics.ListAPIView):
+    queryset = models.Vendor.objects.all()
+    serializer_class = serializers.VendorSerializer
+    
+    
+    
+    
+    
